7e7b5acbc773f6c39d5b5b8dc26f5cc7e6aa14f9
marckesin <192.168.2.1@protonmail.com>
2022-02-26 18:55:21 -0300

    Comentario adicionado para componente linkIcon

--- components/linkIcon.jsx
+++ components/linkIcon.jsx
@@ -1,6 +1,7 @@
 import Link from "@mui/material/Link";
 import styles from "../styles/Home.module.css";
 
+// Link icon component
 export default function LinkIcon({ href, children }) {
   return (
     <Link
