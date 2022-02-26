c49b46cd394b082293683ede0a36f29187444684
marckesin <192.168.2.1@protonmail.com>
2022-02-26 18:48:50 -0300

    Correção de props duplicadas

--- components/social.jsx
+++ components/social.jsx
@@ -1,43 +1,21 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
-import Link from "@mui/material/Link";
+import LinkIcon from "./linkIcon";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import RedditIcon from "@mui/icons-material/Reddit";
-import styles from "../styles/Home.module.css";
 
 // Social icons component
 export default function Social() {
   return (
     <>
-      <Link
-        className={styles.social}
-        href="https://www.github.com/marckesin"
-        color="primary"
-        target="_blank"
-        rel="noopener noreferrer"
-        color="textSecondary"
-      >
+      <LinkIcon href="https://www.github.com/marckesin">
         <GitHubIcon style={{ fontSize: 20 }} />
-      </Link>
-      <Link
-        className={styles.social}
-        href="https://www.linkedin.com/in/marckesin"
-        color="primary"
-        target="_blank"
-        rel="noopener noreferrer"
-        color="textSecondary"
-      >
+      </LinkIcon>
+      <LinkIcon href="https://www.linkedin.com/in/marckesin">
         <LinkedInIcon fontSize="medium" />
-      </Link>
-      <Link
-        className={styles.social}
-        href="https://www.reddit.com/user/marckesin"
-        color="primary"
-        target="_blank"
-        rel="noopener noreferrer"
-        color="textSecondary"
-      >
+      </LinkIcon>
+      <LinkIcon href="https://www.reddit.com/user/marckesin">
         <RedditIcon fontSize="medium" />
-      </Link>
+      </LinkIcon>
     </>
   );
 }
