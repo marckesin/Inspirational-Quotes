47441bd26031f645e0a59abd3bab262916f188aa
marckesin <192.168.2.1@protonmail.com>
2022-05-30 18:33:40 -0300

    Update bootstrap version 5.2.0

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,9 +6,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
           rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
           crossOrigin="anonymous"
         />
       </Head>
