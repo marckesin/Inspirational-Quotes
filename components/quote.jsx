7e43e7e33fba568ee7c0e28fab6d56271393fa05
marckesin <192.168.2.1@protonmail.com>
2022-05-31 20:02:14 -0300

    Alterado cores dos ícones no modo noturno

--- components/quote.jsx
+++ components/quote.jsx
@@ -1,8 +1,8 @@
+import RefreshButton from "./refreshButton";
+import styles from "../styles/Home.module.css";
 import { faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
-import RefreshButton from "./refreshButton";
-import styles from "../styles/Home.module.css";
 
 // Quote component that shows the message
 export default function Quote({ quote }) {
